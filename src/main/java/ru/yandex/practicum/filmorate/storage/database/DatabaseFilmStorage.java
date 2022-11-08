0c48cfc000e7d43bf11b83ec55d319481b871be0
Aleksandr Fedorov <alexsfed@yandex.ru>
2022-11-08 10:20:58 +0400

    fix: забыл } при объединении веток

--- src/main/java/ru/yandex/practicum/filmorate/storage/database/DatabaseFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/database/DatabaseFilmStorage.java
@@ -94,6 +94,8 @@ public class DatabaseFilmStorage implements FilmStorage {
     @Override
     public Collection<Film> getFilmsSearchByDirectorAndTitle(String substring) {
         return filmDao.getFilmsSearchByDirectorAndTitle(substring);
+    }
+
     public void deleteFilm(Long id) {
         Optional<Film> film = filmDao.findFilmById(id);
         filmDao.deleteFilm(film.get());
